be2b45876d9e1c15b672bfef60696a75003b8f8a
Kingyul Kim <youremail@example.com>
2025-12-17 16:19:49 +0900

    Refactor: Transactional 클래스 위 변경
    @Transactional(readOnly = true)가 적용되어 있어서
    POST 요청으로 Todo 데이터를 insert하거나 update하는 과정에서 오류가 발생했습니다.
    
    그래서 각 메서드별로 @Transactional을 분리하여 지정했습니다.

--- src/main/java/org/example/expert/domain/todo/service/TodoService.java
+++ src/main/java/org/example/expert/domain/todo/service/TodoService.java
@@ -19,12 +19,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class TodoService {
 
     private final TodoRepository todoRepository;
     private final WeatherClient weatherClient;
 
+    @Transactional
     public TodoSaveResponse saveTodo(AuthUser authUser, TodoSaveRequest todoSaveRequest) {
         User user = User.fromAuthUser(authUser);
 
@@ -47,10 +47,11 @@ public class TodoService {
         );
     }
 
-    public Page<TodoResponse> getTodos(int page, int size) {
+    @Transactional(readOnly = true)
+    public Page<TodoResponse> getTodos(String weather, int page, int size) {
         Pageable pageable = PageRequest.of(page - 1, size);
 
-        Page<Todo> todos = todoRepository.findAllByOrderByModifiedAtDesc(pageable);
+        Page<Todo> todos = todoRepository.findTodos(weather, pageable);
 
         return todos.map(todo -> new TodoResponse(
                 todo.getId(),
@@ -63,6 +64,7 @@ public class TodoService {
         ));
     }
 
+    @Transactional(readOnly = true)
     public TodoResponse getTodo(long todoId) {
         Todo todo = todoRepository.findByIdWithUser(todoId)
                 .orElseThrow(() -> new InvalidRequestException("Todo not found"));

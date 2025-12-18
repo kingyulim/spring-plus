13d570d3728e1af09ab8860a9e9cd0c97c8e8a09
Kingyul Kim <youremail@example.com>
2025-12-18 16:12:35 +0900

    Feat: queryDSL 기능 추가
    기능 추가 하고 findByIdWithUser 의 jpql을 TodoFindByIdWithUser querydsl로 치환

--- src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
+++ src/main/java/org/example/expert/domain/todo/repository/TodoRepository.java
@@ -11,7 +11,7 @@ import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Optional;
 
-public interface TodoRepository extends JpaRepository<Todo, Long> {
+public interface TodoRepository extends JpaRepository<Todo, Long>, TodoCustomRepository {
 
     //@Query("SELECT t FROM Todo t LEFT JOIN FETCH t.user u ORDER BY t.modifiedAt DESC")
     @Query("""

9f7924cb7dd463981ec94b4d18674fe835918e78
Kingyul Kim <youremail@example.com>
2025-12-18 18:14:22 +0900

    Init: spring sucurity 사용으로 필요한 config 기능 초기생성

---
+++ src/main/java/org/example/expert/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package org.example.expert.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servletapi.SecurityContextHolderAwareRequestFilter;
+
+@Configuration
+@RequiredArgsConstructor
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+public class SecurityConfig {
+    private final JwtFilter jwtFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .addFilterBefore(jwtFilter, SecurityContextHolderAwareRequestFilter.class)
+                .authorizeHttpRequests(auth -> auth
+                        // 회원가입, 로그인은 누구나 접근 가능
+                        .requestMatchers(HttpMethod.POST, "/auth/signup", "/auth/signin").permitAll()
+                        .anyRequest().authenticated() // 나머지는 로그인한 사용자만 접근 가능
+                )
+                .build();
+    }
+
+}

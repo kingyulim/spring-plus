55ce969ab21badaa50ff556ddc36f53e408633e8
Kingyul Kim <youremail@example.com>
2025-12-26 11:40:42 +0900

    Refactor: 필요없는 import 삭제

--- src/main/java/org/example/expert/aop/ManagerRegisterLoggingAspect.java
+++ src/main/java/org/example/expert/aop/ManagerRegisterLoggingAspect.java
@@ -1,6 +1,5 @@
 package org.example.expert.aop;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
